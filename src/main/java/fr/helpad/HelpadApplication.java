54798d16ae827c284826b1499684562c6735cdf9
SALLmouhamadou <salllmouha10@gmail.com>
2023-03-10 16:00:22 +0100

    Commit initial

---
+++ src/main/java/fr/helpad/HelpadApplication.java
@@ -0,0 +1,13 @@
+package fr.helpad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/helpad/HelpadApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.helpad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

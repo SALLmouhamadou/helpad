bb7e6778a5a14f25f09febb33725b5f4819ad668
Pickendorf <seal.dofus@gmail.com>
2023-03-31 10:31:46 +0200

    Import JPARepository

--- src/main/java/fr/helpad/repository/PrescriptionRepository.java
+++ src/main/java/fr/helpad/repository/PrescriptionRepository.java
@@ -2,8 +2,8 @@ package fr.helpad.repository;
 
 import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import fr.helpad.entity.Prescription;

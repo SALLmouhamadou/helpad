f82f766bcd83eaa8649759473087819c0f536ef6
Khalid <khalid91e@hotmail.fr>
2023-03-31 10:24:17 +0200

    classes Nutritionniste

---
+++ src/main/java/fr/helpad/repository/NutritionnisteRepository.java
@@ -0,0 +1,10 @@
+package fr.helpad.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import fr.helpad.entity.Nutritionniste;
+
+public interface NutritionnisteRepository extends JpaRepository<Nutritionniste, Long> {
+	public Nutritionniste findByEmail(String email, String password);
+	
+}

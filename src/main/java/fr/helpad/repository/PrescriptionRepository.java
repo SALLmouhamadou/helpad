aa60f2162340f8d2f2e8fa732b6e4ba96bb54b8e
Pickendorf <seal.dofus@gmail.com>
2023-03-31 10:31:17 +0200

    Correction du calcul de la conso par mois

--- src/main/java/fr/helpad/repository/PrescriptionRepository.java
+++ src/main/java/fr/helpad/repository/PrescriptionRepository.java
@@ -8,12 +8,12 @@ import org.springframework.data.repository.query.Param;
 
 import fr.helpad.entity.Prescription;
 
-public interface PrescriptionRepository extends CrudRepository<Prescription, Long> {
+public interface PrescriptionRepository extends JpaRepository<Prescription, Long> {
 	@Query(value = "SELECT c FROM Prescription c WHERE c.idPensionnaire LIKE '%' || :pensionnaire || '%'")
 	public List<Prescription> chercherParPensionnaire(@Param("pensionnaire") Long idPensionnaire);
 	@Query(value = "SELECT c FROM Prescription c WHERE c.idMedicament LIKE '%' || :medicament || '%'")
-	public List<Prescription> chercherParMedicament(@Param("medicament") Long idMedicament);
-	@Query(value = "SELECT SUM(c.quantiteParPrise * c.priseParJour) FROM Prescription c WHERE c.dateDebutTraitement >= CURRENT_DATE "
+	public List<Prescription> findByMedicament(@Param("medicament") Long idMedicament);
+	@Query(value = "SELECT SUM(c.quantiteParPrise * c.priseParJour * 30) FROM Prescription c WHERE c.dateDebutTraitement >= CURRENT_DATE "
 			+ "AND c.idMedicament = medicament")
 	public Long getSumConsoMois(@Param("medicament") Long idMedicament);
 }
